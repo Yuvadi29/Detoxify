590f9fec604832ec96fc11c815bdf33392256c4c
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-20 11:36:34 +0530

    Saving User Preferences

--- client/app/select-topic/page.js
+++ client/app/select-topic/page.js
@@ -1,11 +1,14 @@
 "use client"
 
-import { useState } from 'react';
+import AuthContext from '@/context/AuthContext';
+import axios from '../../utils/axios';
+import { useContext, useState } from 'react';
 
 const topics = ['DevOps', 'Rust', 'GoLang', 'React', 'Node.js'];
 
 export default function SelectTopic() {
     const [selectedTopics, setSelectedTopics] = useState([]);
+    const { user } = useContext(AuthContext);
 
     const toggleTopic = (topic) => {
         setSelectedTopics((prev) =>
@@ -15,22 +18,37 @@ export default function SelectTopic() {
         );
     };
 
+    const savePreferences = async () => {
+        if (!user) {
+            alert('Please login to save your preferences');
+            return;
+        }
+        try {
+            await axios.post('/topics/select', { topics: selectedTopics });
+            alert('Preferences saved Successfully');
+        } catch (error) {
+            console.log("Failed to save Preferences: ", error);
+        }
+    };
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Select Topics of Interest</h1>
             <div className="grid grid-cols-2 gap-4">
                 {topics.map((topic) => (
-                    <div
+                    <button
                         key={topic}
                         className={`p-4 border rounded cursor-pointer ${selectedTopics.includes(topic) ? 'bg-blue-600 text-white' : 'bg-gray-600'
                             }`}
                         onClick={() => toggleTopic(topic)}
                     >
                         {topic}
-                    </div>
+                    </button>
                 ))}
             </div>
-            <button className="mt-6 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+            <button
+                onClick={savePreferences}
+                className="mt-6 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
                 Save Preferences
             </button>
         </div>
